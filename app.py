8cd2ca05ae2dffbf15d12d846979d1ea060f8acb
Clément <clement.llobet@jasparo.fr>
2023-01-31 17:21:36 +0100

    refactor code with functions

--- app.py
+++ app.py
@@ -4,15 +4,20 @@ url = "https://www.gov.uk/search/news-and-communications"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 
-titles = soup.find_all("a", class_="gem-c-document-list__item-title")
+def findClassInSpecificTag(tag, SearchedClass):
+    return soup.find_all(tag, class_= SearchedClass)
+
+def appendTextInList(src, currentList):
+    for title in src:
+        currentList.append(title.string)
+    return currentList
+
+titles = findClassInSpecificTag("a", "gem-c-document-list__item-title")
 titlesTexts = []
+titlesResult = appendTextInList(titles, titlesTexts)
 
-for title in titles:
-    titlesTexts.append(title.string)
-
-
-descriptions = soup.find_all("p", class_="gem-c-document-list__item-description")
+descriptions = findClassInSpecificTag("p", "gem-c-document-list__item-description")
 descriptionsTexts = []
+descriptionsResult = appendTextInList(descriptions, descriptionsTexts)
 
-for description in descriptions:
-    descriptionsTexts.append(description.string)
+print(descriptionsResult)
