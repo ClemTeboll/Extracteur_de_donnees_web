6c88c1fda02ea35e77e05dc00fdef82b95f64957
Clément <clement.llobet@jasparo.fr>
2023-01-31 17:03:19 +0100

    get titles and descriptions from website

--- app.py
+++ app.py
@@ -3,3 +3,16 @@ from bs4 import BeautifulSoup
 url = "https://www.gov.uk/search/news-and-communications"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
+
+titles = soup.find_all("a", class_="gem-c-document-list__item-title")
+titlesTexts = []
+
+for title in titles:
+    titlesTexts.append(title.string)
+
+
+descriptions = soup.find_all("p", class_="gem-c-document-list__item-description")
+descriptionsTexts = []
+
+for description in descriptions:
+    descriptionsTexts.append(description.string)
